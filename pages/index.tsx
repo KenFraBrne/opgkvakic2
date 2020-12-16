b3edfc00223a31b5b538652ae3b03c36b4de8afe
Marko Kvakic <markokvakic@gmail.com>
2020-12-16 19:18:32 +0100

    addition : home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 import Fade from 'react-reveal/Fade'
 import MainLayout from 'layout/MainLayout'
+import UnderlinedLink from 'component/element/UnderlinedLink'
 import style from 'pages/style.module.css'
 
 export default function HomePage() {
   return (
     <MainLayout>
 
-      <div className={ style.container }>
+      <div className={ style.vidContainer }>
 
         <div className={ style.background }>
           <video autoPlay muted loop>
@@ -15,20 +16,13 @@ export default function HomePage() {
           </video>
         </div>
 
-        <div
-          className={ style.foreground }
-          style={{
-            color: 'white',
-            background: 'rgba( var(--mainColor), 0.8 )',
-          }}>
+        <div className={ style.foreground }>
           <div>
-            <Fade right>
+            <Fade right duration={1500}>
               <h1> Dobrodošli </h1>
               <p> Curabitur tempus mattis quam. Integer purus diam, ultrices consequat posuere. </p>
-            </Fade>
-            <Fade right duration={2000}>
               <Link href="/order" passHref>
-                <div><a> Naručite </a></div>
+                <a> Naručite </a>
               </Link>
             </Fade>
           </div>
@@ -36,15 +30,16 @@ export default function HomePage() {
 
       </div>
 
-      <div className={ style.container }>
+
+      <div className={ style.imgContainer }>
 
         <div className={ style.background }>
           <img src="/home/rezel-apacionado-MZfS19xrrz0-unsplash.png"/>
         </div>
 
-        <div className={ style.foreground } style={{ justifyContent: 'flex-end', }}>
-          <div style={{ background: 'rgba( 255, 255, 255, 0.8 )'}}>
-            <Fade left>
+        <div className={ style.foreground }>
+          <div>
+            <Fade left duration={1500}>
               <h1> Upoznajte nas </h1>
               <p> Sed aliquet diam sit amet facilisis semper. Cras luctus ligula vitae porta suscipit. </p>
               <p> Donec sit amet est tempus risus ultricies vulputate. Suspendisse hendrerit. </p>
